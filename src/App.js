a70128aea9890e3167c0d4873aea0e8779e5d557
Ibrahim AlRayyan <ibrahim.alrayyan@mecaasoft.com>
2022-11-18 16:37:40 +0200

    Use Themeprovider from styled

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import BasicTitle from './components/BasicTile';
-import ComplexTitle from './components/ComplexTitle';
-import { DefaultButton, HipsterButton } from './components/DefaultButton';
-import Card from './components/Card';
+
+import { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import { DefaultButton } from './components/DefaultButton';
 import GlobalStyle from './components/GlobalStyle';
 
+const baseTheme = {
+  color: "#222",
+  background: "#fff",
+}
+
+const darkTheme = {
+  color: "#fff",
+  background: "#222",
+}
+
+const ContainerToTestThemProvider = styled.div`
+  padding: 2rem;
+  color: ${(props) => props.theme.color};
+  background: ${(props) => props.theme.background};
+  border-radius: 0.35rem;
+  box-shadow: 0 2px 2px #666;
+`;
+
 function App() {
+  const [isBaseTheme, toggleTheme] = useState(true);
+  const toggleThemeHandler = () => toggleTheme(isBaseTheme => !isBaseTheme);
+
   return (
-    <div style={{ padding: "2rem" }}>
-      <GlobalStyle />
-      <h2>Card will be here!</h2>
-      <Card />
-    </div>
+    <ThemeProvider theme={isBaseTheme ? baseTheme : darkTheme}>
+      <div style={{ padding: "2rem" }}>
+        <GlobalStyle />
+        <ContainerToTestThemProvider>
+          <h2>Theme Provider</h2>
+          <DefaultButton onClick={toggleThemeHandler}>Toggle me!</DefaultButton>
+        </ContainerToTestThemProvider>
+      </div>
+    </ThemeProvider>
   );
 }
 
