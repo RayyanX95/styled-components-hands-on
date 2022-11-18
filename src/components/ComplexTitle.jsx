8fc8e0d6187324f8ece7140f61ade099d80dca7b
Ibrahim AlRayyan <ibrahim.alrayyan@mecaasoft.com>
2022-11-18 11:13:31 +0200

    use global css within styled

--- src/components/ComplexTitle.jsx
+++ src/components/ComplexTitle.jsx
@@ -1,47 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// const ComplexTitle = ({ title }) => {
-//   return (
-//     <Wrapper>
-//       <h1>{title}</h1>
-//       <div className="underline"></div>
-//     </Wrapper>
-//   );
-// };
-
-// const Wrapper = styled.div`
-//   h1 {
-//     text-transform: capitalize;
-//     text-align: center;
-//   }
-
-//   .underline {
-//     width: 5rem;
-//     height: .25rem;
-//     background: blueviolet;
-//     margin: 0 auto;
-//   }
-// `
-
-// export default ComplexTitle;
-
-
-// ----------------------------------------
-
-
-//* Another way to apply styled to a React component:
-
-const ComplexTitle = ({ title, className: classNameFromStyled }) => {
+const ComplexTitle = ({ title }) => {
   return (
-    <div className={classNameFromStyled}>
+    <Wrapper>
       <h1>{title}</h1>
       <div className="underline"></div>
-    </div>
+    </Wrapper>
   );
 };
 
-const Wrapper = styled(ComplexTitle)`
+const Wrapper = styled.div`
   h1 {
     text-transform: capitalize;
     text-align: center;
@@ -53,6 +22,37 @@ const Wrapper = styled(ComplexTitle)`
     background: blueviolet;
     margin: 0 auto;
   }
-  `
+`
 
-export default Wrapper;
+export default ComplexTitle;
+
+
+// ----------------------------------------
+
+
+//* Another way to apply styled to a React component:
+
+// const ComplexTitle = ({ title, className: classNameFromStyled }) => {
+//   return (
+//     <div className={classNameFromStyled}>
+//       <h1>{title}</h1>
+//       <div className="underline"></div>
+//     </div>
+//   );
+// };
+
+// const Wrapper = styled(ComplexTitle)`
+//   h1 {
+//     text-transform: capitalize;
+//     text-align: center;
+//   }
+
+//   .underline {
+//     width: 5rem;
+//     height: .25rem;
+//     background: blueviolet;
+//     margin: 0 auto;
+//   }
+//   `
+
+// export default Wrapper;
