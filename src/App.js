c4a66ef5078cc2832438dc44ed1ce0e71f03138a
Ibrahim AlRayyan <ibrahim.alrayyan@mecaasoft.com>
2022-11-18 16:47:59 +0200

    Use styled animation and keyframes to build a Loading spinner

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 
-import { useState } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { DefaultButton } from './components/DefaultButton';
 import GlobalStyle from './components/GlobalStyle';
-
-const baseTheme = {
-  color: "#222",
-  background: "#fff",
-}
-
-const darkTheme = {
-  color: "#fff",
-  background: "#222",
-}
-
-const ContainerToTestThemProvider = styled.div`
-  padding: 2rem;
-  color: ${(props) => props.theme.color};
-  background: ${(props) => props.theme.background};
-  border-radius: 0.35rem;
-  box-shadow: 0 2px 2px #666;
-`;
-
+import Loading from './components/Loading';
 function App() {
-  const [isBaseTheme, toggleTheme] = useState(true);
-  const toggleThemeHandler = () => toggleTheme(isBaseTheme => !isBaseTheme);
 
   return (
-    <ThemeProvider theme={isBaseTheme ? baseTheme : darkTheme}>
-      <div style={{ padding: "2rem" }}>
-        <GlobalStyle />
-        <ContainerToTestThemProvider>
-          <h2>Theme Provider</h2>
-          <DefaultButton onClick={toggleThemeHandler}>Toggle me!</DefaultButton>
-        </ContainerToTestThemProvider>
-      </div>
-    </ThemeProvider>
+    <div style={{ padding: '2rem'}}>
+      <GlobalStyle />
+      <Loading />
+    </div>
   );
 }
 
