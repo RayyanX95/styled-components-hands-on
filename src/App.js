ce67751a8d110f599528a33945352ba7c6db26fa
Ibrahim AlRayyan <ibrahim.alrayyan@mecaasoft.com>
2022-11-18 10:35:23 +0200

    add hispter button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import BasicTitle from './components/BasicTile';
-import DefaultButton from './components/DefaultButton';
+import { DefaultButton, HipsterButton } from './components/DefaultButton';
 
 function App() {
   return (
-    <div style={{ padding:"2rem"}}>
+    <div style={{ padding: "2rem" }}>
       <BasicTitle>Styled Title</BasicTitle>
       <BasicTitle color="red">Styled Title</BasicTitle>
       <BasicTitle color="blue">Styled Title</BasicTitle>
       <BasicTitle color="green">Styled Title</BasicTitle>
       <DefaultButton>Click me :)</DefaultButton>
+      <HipsterButton>Hipster Button 🥳</HipsterButton>
     </div>
   );
 }
--- src/components/DefaultButton.jsx
+++ src/components/DefaultButton.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const DefaultButton = styled.button`
+export const DefaultButton = styled.button`
   background: blueviolet;
   color: white;
   padding: .7rem;
@@ -8,6 +8,16 @@ const DefaultButton = styled.button`
   width: 200px;
   margin: 1rem auto;
   border: none;
+  cursor: pointer;
 `;
 
-export default DefaultButton;
+export const HipsterButton = styled(DefaultButton)`
+  width: 400px;
+  background: transparent;
+  color: blueviolet;
+  border: 1px solid blueviolet;
+  &:hover {
+    background-color: blueviolet;
+    color: white;
+  }
+`
